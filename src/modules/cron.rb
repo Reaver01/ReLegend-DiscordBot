02df61a9256f1a1da19a1be2fdbd928a125ab5e6
Reaver01 <jawmac@gmail.com>
2017-08-17 13:36:54 -0500

    Update cron.rb

--- src/modules/cron.rb
+++ src/modules/cron.rb
@@ -7,7 +7,7 @@ module Bot
       project = KICKSCRAPER.search_projects('Re:Legend - Co-op Monster-Raising RPG').first
         
       # Set game text
-      BOT.game = "S$ #{project.pledged.to_i} pledged!"
+      BOT.game = "S$ #{project.pledged.to_i.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse} pledged!"
     end
 
     # Does something every 30 mins
