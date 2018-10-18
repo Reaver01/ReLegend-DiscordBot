0157d32c3573d5b870b82dcf0bbc91ea986ab6d0
TriZ0n <ts.trizon@gmail.com>
2018-10-18 13:11:03 -0500

    Fix welcome message
    Ban users that are either advertising or attacking other discord server with links in their username.

--- src/modules/events/member_join.rb
+++ src/modules/events/member_join.rb
@@ -5,7 +5,12 @@ module Bot
       extend Discordrb::EventContainer
       member_join do |event|
         if Database::Server.resolve_id(event.server.id).has_welcome_message
-          BOT.channel(event.server.id).send_message(event.user.mention + Database::Server.resolve_id(event.server.id).welcome_message) 
+          if event.user.username =~ /.*(discord\.gg\/\w+)/ || event.user.display_name =~ /.*(discord\.gg\/\w+)/
+            # User is either advertising or attacking another server, we'll ban them.
+            event.server.ban(event.user)
+          else
+            BOT.channel(event.server.id).send_message(event.user.mention + ' ' + Database::Server.resolve_id(event.server.id).welcome_message)
+          end
         end
       end
     end
