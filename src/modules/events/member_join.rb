242139ba7734c159627412b2f90a2d52e46a6059
TriZ0n <ts.trizon@gmail.com>
2018-10-22 18:45:30 -0500

    Update advertising bans.
    Ban usernames with Discord.me, Twitter and Twitch links.

--- src/modules/events/member_join.rb
+++ src/modules/events/member_join.rb
@@ -8,6 +8,12 @@ module Bot
           if event.user.username =~ /.*(discord\.gg\/\w+)/ || event.user.display_name =~ /.*(discord\.gg\/\w+)/
             # User is either advertising or attacking another server, we'll ban them.
             event.server.ban(event.user)
+          elsif event.user.username =~ /.*(discord\.me\/\w+)/ || event.user.display_name =~ /.*(discord\.me\/\w+)/
+            event.server.ban(event.user)
+          elsif event.user.username =~ /.*(twitter\.com)/ || event.user.display_name =~ /.*(twitter\.com)/
+            event.server.ban(event.user)
+          elsif event.user.username =~ /.*(twitch\.tv)/ || event.user.display_name =~ /.*(twitch\.tv)/
+            event.server.ban(event.user)
           else
             BOT.channel(event.server.id).send_message(event.user.mention + ' ' + Database::Server.resolve_id(event.server.id).welcome_message)
           end
